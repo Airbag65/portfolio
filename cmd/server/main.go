f3a8c0b657934265abd86a6e0e0105b8bd056855
Anton Norman <normananton03@gmail.com>
2025-11-19 20:02:43 +0100

    Starting project

---
+++ cmd/server/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func main() {
+	templ, err := template.ParseGlob("./public/views/*.html")
+	if err != nil {
+		log.Fatalf("Could not parse: %v\n", err)
+	}
+
+	e := echo.New()
+	e.Renderer = &TemplateRenderer{
+		templates: templ,
+	}
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index.html", nil)
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
