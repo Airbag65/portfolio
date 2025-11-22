0ce46c83c90f97799d55f57e36bf525141a4c545
Anton Norman <normananton03@gmail.com>
2025-11-22 16:00:20 +0100

    Getting data to site from server. projects page started

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,11 +9,33 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type Project struct {
+	Title       string
+	Tags        []string
+	Description string
+	Link        string
+	Id          int
+}
+
+type Projects struct {
+	Items []Project
+}
+
+func NewMockProject() Project {
+	return Project{
+		Title:       "Portfolio",
+		Tags:        []string{"Golang", "HTMX"},
+		Description: "Portfolio to showcase my projects and expertice",
+		Link:        "https://www.github.com/Airbag65/portfolio",
+		Id:          1,
+	}
+}
+
 type TemplateRenderer struct {
 	templates *template.Template
 }
 
-func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+func (t *TemplateRenderer) Render(w io.Writer, name string, data any, c echo.Context) error {
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
@@ -28,14 +50,18 @@ func main() {
 		templates: templ,
 	}
 
+	project := NewMockProject()
+	projects := Projects{
+		Items: []Project{project},
+	}
+
 	e.GET("/", func(c echo.Context) error {
 		return c.Render(http.StatusOK, "index.html", nil)
 	})
 
-	e.POST("/testing", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "test.html", nil)
+	e.POST("/projects", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "projects.html", projects)
 	})
-
 
 	e.POST("/about", func(c echo.Context) error {
 		return c.Render(http.StatusOK, "about.html", nil)
@@ -45,6 +71,10 @@ func main() {
 		return c.Render(http.StatusOK, "start.html", nil)
 	})
 
+	e.POST("/project/:id", func(c echo.Context) error {
+		id := c.Param("id")
+		return c.Render(http.StatusOK, "project.html", id)
+	})
+
 	e.Logger.Fatal(e.Start(":8080"))
 }
-
